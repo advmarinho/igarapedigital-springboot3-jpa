8d23a1cd5387b5890c6f85ec78dcb90c95ac4620
andersons <marinho.1983@gmail.com>
2024-01-14 11:26:44 -0300

    Aula 302 Curso Java usando o postman BD

---
+++ webcurso/src/main/java/com/igarapedigital/webcurso/services/UserService.java
@@ -0,0 +1,30 @@
+package com.igarapedigital.webcurso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igarapedigital.webcurso.entities.User;
+import com.igarapedigital.webcurso.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+		
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+	
+	
+}
