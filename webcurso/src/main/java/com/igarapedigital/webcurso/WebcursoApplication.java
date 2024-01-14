d26a6a2dca98a61d48254077ec75b1e16dfdded1
andersons <marinho.1983@gmail.com>
2024-01-13 23:55:55 -0300

    Inicio Projeto

---
+++ webcurso/src/main/java/com/igarapedigital/webcurso/WebcursoApplication.java
@@ -0,0 +1,13 @@
+package com.igarapedigital.webcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebcursoApplication.class, args);
+	}
+
+}
---
+++ webcurso/src/test/java/com/igarapedigital/webcurso/WebcursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igarapedigital.webcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebcursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
